5233cb698c790d302ecece473124fe940f09d21d
atom10 <atom10.kw10@gmail.com>
2023-10-22 23:06:17 +0200

    Initial commit

---
+++ src/main/java/com/capybarainc/BookStore/Controllers/Index.java
@@ -0,0 +1,12 @@
+package com.capybarainc.BookStore.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Index {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
