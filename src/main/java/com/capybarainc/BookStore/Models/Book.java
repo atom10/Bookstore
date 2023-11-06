8e78b83223b108bea46639a123c7d0bdffe692b7
atom10 <atom10.kw10@gmail.com>
2023-11-06 10:50:09 +0100

    Add simple repository

---
+++ src/main/java/com/capybarainc/BookStore/Models/Book.java
@@ -0,0 +1,24 @@
+package com.capybarainc.BookStore.Models;
+
+// Importing required classes
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+}
